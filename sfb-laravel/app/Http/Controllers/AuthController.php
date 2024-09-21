2c778b5da9591d4c2a074916c777c44435958ff9
Bryan <54081465+arthvadrr@users.noreply.github.com>
2024-09-21 11:30:15 -0500

    Learning laravel stuffs

--- sfb-laravel/app/Http/Controllers/AuthController.php
+++ sfb-laravel/app/Http/Controllers/AuthController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponses;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login()
+
+    public function login(ApiLoginRequest $request)
     {
-        return $this->ok('Hello Matey');
+        return $this->ok($request->get('email'));
+    }
+
+    public function register(ApiLoginRequest $request)
+    {
+        return $this->ok('register');
     }
 }
